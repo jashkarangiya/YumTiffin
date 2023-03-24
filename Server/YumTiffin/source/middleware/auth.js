45bc31a2732f637a6a944b39138f8db9415e9608
jashkarangiya <jashkarangiya@gmail.com>
2023-03-24 21:22:21 +0530

    Complete auth file

---
+++ Server/YumTiffin/source/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const Register = require('../models/registers');
+
+const auth = async(req, res, next) => {
+    try {
+        console.log("Hello from auth");
+        const token = req.cookies.jwt;
+        const verifyUser = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(verifyUser);
+
+        const user = await Register.findOne({ _id: verifyUser._id });
+        console.log(user.firstName);
+
+        req.token = token;
+        req.user = user;
+
+
+        next();
+
+    } catch (error) {
+        res.status(401).send(error);
+    }
+}
+
+module.exports = auth;
